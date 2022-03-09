6811177d8cf7dc1d0ba29ca9e325f466f3d8aa08
Jerzy <jerzysz@outlook.de>
2022-03-09 18:45:52 +0100

    Added QB Target Option

--- config.lua
+++ config.lua
@@ -4,4 +4,17 @@ Config.useTarget = false	    -- Use QB Target to enable the billing
 Config.useCommand = true	-- Use QB Target to enable the billing
 Config.useCash = false		-- Abillity to use cash for billing/ If false then Banking
 
--- More features comming soon
+-- Add this in your QB-Target Init.lua and set useTraget to true
+
+--[[
+    Config.GlobalPlayerOptions = {
+        options = {
+            {
+            type = "client",
+            event = "jerzys_billing:client:openBillingMenu",
+            icon = 'fas fa-phone',
+            label = 'Generate a bill',
+            },
+        }
+    }
+]]--
